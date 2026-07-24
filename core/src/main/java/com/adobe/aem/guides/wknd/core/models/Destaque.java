7e4ff2af5765f4e9abd6c8ad40eba78da146e314
gustavodanjos <gustavoanjos.aluno@unipampa.edu.br>
2026-07-24 00:26:13 -0300

    feat(core): define interface do Sling Model Destaque

---
+++ core/src/main/java/com/adobe/aem/guides/wknd/core/models/Destaque.java
@@ -0,0 +1,17 @@
+package com.adobe.aem.guides.wknd.core.models;
+
+import org.osgi.annotation.versioning.ProviderType;
+
+@ProviderType
+public interface Destaque {
+
+    String getTitle();
+
+    String getText();
+
+    String getButtonText();
+
+    String getButtonUrl();
+
+    boolean isMostrarBotao();
+}
