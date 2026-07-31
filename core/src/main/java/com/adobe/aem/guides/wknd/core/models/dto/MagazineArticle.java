4b145bb3064b4e1a4d5e7442807ffc3211a6488c
gustavodanjos <gustavoanjos.aluno@unipampa.edu.br>
2026-07-31 10:06:04 -0300

    feat(core): cria DTO MagazineArticle para representação de artigos

---
+++ core/src/main/java/com/adobe/aem/guides/wknd/core/models/dto/MagazineArticle.java
@@ -0,0 +1,72 @@
+package com.adobe.aem.guides.wknd.core.models.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class MagazineArticle {
+
+    @JsonProperty("title")
+    private String title;
+
+    @JsonProperty("path")
+    private String path;
+
+    @JsonProperty("imagePath")
+    private String imagePath;
+
+    @JsonProperty("description")
+    private String description;
+
+    @JsonProperty("lastModified")
+    private String lastModified;
+
+    public MagazineArticle() {
+    }
+
+    public MagazineArticle(String title, String path, String imagePath, String description, String lastModified) {
+        this.title = title;
+        this.path = path;
+        this.imagePath = imagePath;
+        this.description = description;
+        this.lastModified = lastModified;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLastModified() {
+        return lastModified;
+    }
+
+    public void setLastModified(String lastModified) {
+        this.lastModified = lastModified;
+    }
+}
